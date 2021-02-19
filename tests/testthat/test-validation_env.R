be86a5c5c692622fb353f1077d918a19b3a1a43d
Marie Vendettuoli <mvendett@scharp.org>
2021-02-18 20:46:31 -0800

    fixes issue #10

--- tests/testthat/test-validation_env.R
+++ tests/testthat/test-validation_env.R
@@ -40,24 +40,28 @@ test_that("temp", {
                              validation_env1[validation_env1$type == "extended_req", "resource"])
   # Suggest + Depends
   depends_field <- "tidyverse"
-  fields$Depends <- depends_field
-  desc_contents2 <- desc::desc(text = glue::glue("{names(fields)}: {fields}"))
-  writeLines(con = normalizePath(file.path(temp_path, "DESCRIPTION")),
+  if(depends_field %in% row.names(installed.packages())){
+    fields$Depends <- depends_field
+    desc_contents2 <- desc::desc(text = glue::glue("{names(fields)}: {fields}"))
+    writeLines(con = normalizePath(file.path(temp_path, "DESCRIPTION")),
              text = desc_contents2$str(by_field = TRUE, normalize = FALSE, mode = "file"))
-  validation_env2 <- vt_scrape_val_env(temp_path)
-  testthat::expect_equal(3, nrow(validation_env2[validation_env2$type == "extended_req",]))
-  testthat::expect_equal(depends_field, validation_env2[validation_env2$type == "package_req", "resource"])
+    validation_env2 <- vt_scrape_val_env(temp_path)
+    testthat::expect_equal(3, nrow(validation_env2[validation_env2$type == "extended_req",]))
+    testthat::expect_equal(depends_field, validation_env2[validation_env2$type == "package_req", "resource"])
+  }
 
   # all
   imports_field <- "magrittr, plotly"
-  fields$Imports <- imports_field
-  desc_contents3 <- desc::desc(text = glue::glue("{names(fields)}: {fields}"))
-  writeLines(con = normalizePath(file.path(temp_path, "DESCRIPTION")),
+  if(all(strsplit(imports_field, split = ", ")[[1]] %in% row.names(installed.packages()))){
+    fields$Imports <- imports_field
+    desc_contents3 <- desc::desc(text = glue::glue("{names(fields)}: {fields}"))
+    writeLines(con = normalizePath(file.path(temp_path, "DESCRIPTION")),
              text = desc_contents3$str(by_field = TRUE, normalize = FALSE, mode = "file"))
-  validation_env3 <- vt_scrape_val_env(temp_path)
-  testthat::expect_equal(3, nrow(validation_env3[validation_env3$type == "extended_req",]))
-  testthat::expect_equal(c(strsplit(imports_field, split = ", ")[[1]], depends_field),
+    validation_env3 <- vt_scrape_val_env(temp_path)
+    testthat::expect_equal(3, nrow(validation_env3[validation_env3$type == "extended_req",]))
+    testthat::expect_equal(c(strsplit(imports_field, split = ", ")[[1]], depends_field),
                          validation_env3[validation_env3$type == "package_req", "resource"])
+  }
 
   # package that doesn't exist
   fake_pkg <- "myFakePackage"
