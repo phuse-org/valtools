a2f3f54ff2b79fe0bd406296c274c51c19f1d152
Marie Vendettuoli <mvendett@scharp.org>
2021-02-22 14:16:07 -0800

    add dd-MMM-YYYY

--- tests/testthat/test-roxygen_extension.R
+++ tests/testthat/test-roxygen_extension.R
@@ -63,3 +63,36 @@ test_that("Roxygen can read in new tags - MM-DD-YYYY", {
   )
 
 })
+
+test_that("Roxygen can read in new tags - DD-MMMM-YYYY", {
+
+  simple_test_function_text <- "
+  #' @title simple title
+  #' @description sample function
+  #' @param pkg path to package
+  #' @editor Sample Person
+  #' @editDate 10-Jan-2021
+  #' @export
+  #' @aliases testing
+  hello_world <- function(name = \"\"){
+    paste(\"Hello,\",name)
+  }
+  "
+
+  roxygen_block <- roxygen2::parse_text(simple_test_function_text)[[1]]
+
+  expect_true(
+    roxygen2::block_has_tags(roxygen_block, tags = c("editor","editDate"))
+  )
+
+  expect_equal(
+    roxygen2::block_get_tag_value(roxygen_block,"editor"),
+    "Sample Person"
+  )
+
+  expect_equal(
+    roxygen2::block_get_tag_value(roxygen_block,"editDate"),
+    "2021-01-10"
+  )
+
+})
