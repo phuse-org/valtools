cb26182c9ab56b049101c848e73c8e309e98774b
elimillera <elimillera@gmail.com>
2021-03-01 18:54:36 +0000

    Update importFrom location

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,3 @@
-#' @importFrom usethis edit_file
-NULL
 .onLoad <- function(libname, pkgname) {
   options("vt.validation_directory" = "vignettes/validation")
   options("vt.validation_output_directory" = "inst/validation")
