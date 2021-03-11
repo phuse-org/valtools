1928a6423367bb3f9387150108ff6a16455442a5
elimillera <elimillera@gmail.com>
2021-03-11 16:16:55 +0000

    Updates per #58 Comments: - knitr moved from suggests to imports - kableExtra, purrr, magrittr added to imports - sig_table functions changed to pull usernames from validation config and use a function for kable defaults. - usethis::use_pipe() was run to add standard magrittr import file.

--- R/sig_table.R
+++ R/sig_table.R
@@ -1,17 +1,15 @@
 #' Generate a signature table for a validation report
 #'
-#' @param people data frame with variables: role, name, and title
-#'
-#' @return A data frame with people info formatted for validation report with
-#'   the
+#' @return A dataframe created from the validation config containing a row for
+#'   each user with the columns: role, name_and_title, signature, and date.
 #'
 #' @export
-vt_generate_sig_table <- function(people){
-  # check that the tables have correct variables
-  if (!all(c("role", "name", "title") %in% tolower(names(people)))) {
-    stop(paste0("people table must have variables: role, name, and title. ",
-                "Contains: ", paste0(tolower(names(people)), collapse = ", ")))
-  }
+#'
+#' @importFrom purrr map_dfr
+vt_generate_sig_table <- function(){
+
+  # Pull all the usernames into a data.frame
+  people <- map_dfr(read_validation_config()$usernames, data.frame)
 
   people$signature <- NA
   people$date <- NA
@@ -21,20 +19,51 @@ vt_generate_sig_table <- function(people){
 
 }
 
+#' Kable defaults for rendering validation report
+#'
+#' @param people A dataframe with the columns role, Name and Title, Signature,
+#'   and "Date"
+#'
+#' @return Output from calls to kable
+#'
+#' @export
+#'
+#' @importFrom knitr kable
+#' @importFrom kableExtra kable_styling row_spec column_spec
+#' @importFrom grDevices rgb
+vt_kable_sig_table <- function(people) {
+
+  # check that the tables have correct variables
+  if (!all(c("role", "Name and Title", "Signature", "Date") %in% tolower(names(people)))) {
+    stop(paste0("people table must have variables: role, Name and Title, Signature, and Date ",
+                "Contains: ", paste0(tolower(names(people)), collapse = ", ")))
+  }
+
+  people %>%
+    kable(col.names = c("Role", "Name and Title", "Signature", "Date"),
+          escape = FALSE, booktabs = FALSE) %>%
+    kable_styling(full_width = FALSE, position = "left") %>%
+    row_spec(0, background = rgb(184, 204, 228, maxColorValue = 255)) %>%
+    column_spec(1, width = "9em", border_left = TRUE) %>%
+    column_spec(2, width = "11em") %>%
+    column_spec(3, width = "15em") %>%
+    column_spec(4, width = "8em", border_right = TRUE)
+
+}
+
 #' Return the templated code for inclusion in the validation report
 #'
-#' @param people data frame with variables: role, name, and title
 #' @param template Name of the template to render
 #' @param output directory passed to `render_template()`
 #'
 #' @return The text to include in a validation R markdown document
 #'
 #' @export
-vt_render_sig_table <- function(people, template = "sig_table.Rmd", output) {
+vt_render_sig_table <- function(template = "sig_table.Rmd", output) {
+  output <- vt_set_ext(output, "Rmd")
   render_template(template = template,
-                  data = list(
-                    people = deparse1(people)
-                  ),
                   output = output
   )
 }
+
+
--- tests/testthat/test-sig_table.R
+++ tests/testthat/test-sig_table.R
@@ -1,48 +1,48 @@
 test_that("vt_generate_sig_table generates the expected dataframe", {
-  expect_equal(
-    vt_generate_sig_table(
+
+  withr::with_tempdir({
+
+    vt_create_package(".", open = FALSE)
+
+    vt_add_user_to_config(username = "em", name = "Eli Miller", role = "dev", title = "Developer")
+    vt_add_user_to_config(username = "mv", name = "Marie", role = "dev", title = "Developer")
+    vt_add_user_to_config(username = "eh", name = "Ellis H", role = "dev", title = "Developer")
+
+    expect_equal(
+      vt_generate_sig_table(),
       data.frame(
-        name = c("Eli Miller", "Marie", "Ellis H"),
         role = c("dev", "dev", "dev"),
-        title = c("Developer", "Developer", "Developer")
+        name_and_title = c("Eli Miller, Developer",
+                           "Marie, Developer",
+                           "Ellis H, Developer"),
+        signature = NA,
+        date = NA
       )
-    ),
-    data.frame(
-      role = c("dev", "dev", "dev"),
-      name_and_title = c("Eli Miller, Developer",
-                         "Marie, Developer",
-                         "Ellis H, Developer"),
-      signature = NA,
-      date = NA
     )
-  )
+
+  })
 })
 
 test_that("`vt_render_sig_table` returns the expected RMD text", {
   withr::with_tempdir({
+
+    vt_create_package(".", open = FALSE)
+
+    vt_add_user_to_config(username = "em", name = "Eli Miller", role = "dev", title = "Developer")
+    vt_add_user_to_config(username = "mv", name = "Marie", role = "dev", title = "Developer")
+    vt_add_user_to_config(username = "eh", name = "Ellis H", role = "dev", title = "Developer")
+
     vt_render_sig_table(
-      data.frame(
-        name = c("Eli Miller", "Marie", "Ellis H"),
-        role = c("dev", "dev", "dev"),
-        title = c("Developer", "Developer", "Developer")
-      ),
       template = "sig_table.Rmd",
       output = "rendered_table"
     )
 
-    expect_equal(readLines("rendered_table"),
+    expect_equal(readLines("rendered_table.Rmd"),
                  c("# Signatures",
                    "",
                    "```{r, sig_table, echo = FALSE}",
-                   "structure(list(name = c(&quot;Eli Miller&quot;, &quot;Marie&quot;, &quot;Ellis H&quot;), role = c(&quot;dev&quot;, &quot;dev&quot;, &quot;dev&quot;), title = c(&quot;Developer&quot;, &quot;Developer&quot;, &quot;Developer&quot;)), class = &quot;data.frame&quot;, row.names = c(NA, -3L)) %>%",
-                   "  vt_generate_sig_table() %>%",
-                   "  kable(col.names = c(\"Role\", \"Name and Title\", \"Signature\", \"Date\"),",
-                   "        escape = FALSE, booktabs = FALSE) %>%",
-                   "  kable_styling(full_width = FALSE, position = \"left\") %>%",
-                   "  row_spec(0, background = rgb(184, 204, 228, maxColorValue = 255)) %>%",
-                   "  column_spec(1, width = \"9em\", border_left = TRUE) %>%",
-                   "  column_spec(2, width = \"11em\") %>%", "  column_spec(3, width = \"15em\") %>%",
-                   "  column_spec(4, width = \"8em\", border_right = TRUE)",
+                   "vt_generate_sig_table() %>%",
+                   "  vt_kable_sig_table()",
                    "```"
                  ))
 
@@ -50,15 +50,15 @@ test_that("`vt_render_sig_table` returns the expected RMD text", {
 
 })
 
-test_that("`vt_generate_sig_table` errors when not given expected columns", {
+test_that("`vt_kable_sig_table` errors when not given expected columns", {
 
   expect_error({
-    vt_generate_sig_table(
+    vt_kable_sig_table(
       data.frame(
         role = "aRole",
         name = "Eli"
         )
     )
-  }, c("people table must have variables: role, name, and title. Contains: role, name"))
+  }, c("people table must have variables: role, Name and Title, Signature, and Date "))
 
 })
