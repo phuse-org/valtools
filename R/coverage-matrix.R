d4fff8f30b94d1977b1b3243f07c9a76c182eeb4
Marie Vendettuoli <mvendett@scharp.org>
2021-03-19 09:41:12 -0700

    add wide matrix option

--- R/coverage-matrix.R
+++ R/coverage-matrix.R
@@ -1,29 +1,79 @@
+#' Scrape "coverage" tag in test code to generate mapping
 #' @param references dynamic reference holder if it already exists
 #' @param src,ref passed to \code{\link{vt_scrape_tags_from}}
+#' @param type one of "long" or "wide" which determines shape of output table
+#' @return a data.frame
+#' @importFrom rlang list2 := !!
 #' @export
-vt_scrape_coverage_matrix <- function(references = NULL, src = ".", ref = vt_path()){
-  #TODO some code to tease out what are the test_case files.
-  # only test case have @coverage, maybe trycatch will be enough?
+vt_scrape_coverage_matrix <- function(type = c("long", "wide"), references = NULL, src = ".", ref = vt_path()){
 
-  cov_raw_values <- sapply(vt_scrape_tags_from(type = "test_cases", tags = "coverage"),
-                           FUN = function(x){x$coverage})
-
+  ## instantiate dynamic referencing
+  ## if no dynamic reference used, this won't get populated
   if(is.null(references)){
     references <- vt_dynamic_referencer$new()
   }
+
+  ## helper functions
+
+  split_vals <- function(x){
+    this_row <- strsplit(x, split = ":")[[1]]
+    names(this_row) <- c("req_id", "tc_id")
+    this_row["tc_id"] <- trimws(this_row["tc_id"])
+
+    this_row
+  }
+
+  split_tc <- function(vals){
+    out <- do.call("rbind", apply(vals, 1, FUN = function(x){
+      data.frame(req_id = x[["req_id"]],
+                  tc_id = strsplit(x[["tc_id"]], ", ")[[1]],
+                  check.rows = FALSE)
+    }))
+    row.names(out) <- 1:nrow(out)
+    out <- out[order(out$req_id),]
+    out
+  }
+
+
+
+  # avoids dependency on tidyr::pivot_wider
+  make_wider <- function(long_vals){
+    list_x <- apply(long_vals, 1, FUN = function(x){
+      as.data.frame(list2(req_id = x[["req_id"]],
+                          !!x[["tc_id"]] := "x"),
+                    check.names = FALSE)
+    })
+
+    all_names <- unique(unlist(lapply(list_x, names)))
+    list_all_x <- lapply(list_x, function(x){
+      x[setdiff(all_names, names(x))] <- ""
+      x
+    })
+    out <- do.call("rbind", list_all_x)
+    row.names(out) <- 1:nrow(out)
+    out[, c("req_id", sort(names(out)[-1]))]
+  }
+
+
+  ## end helper functions
+
+  cov_raw_values <- sapply(vt_scrape_tags_from(type = "test_cases", tags = "coverage",
+                                               src = src, ref = vt_path()),
+                           FUN = function(x){x$coverage})
   references$scrape_references(cov_raw_values)
-  numbered_cov_vals <- as.data.frame(do.call("rbind", lapply(
-    unlist(lapply(references$reference_insertion(cov_raw_values),
-            strsplit, split = "\n")),
-    FUN = function(x){
-      this_row <- strsplit(x, split = ":")[[1]]
-      names(this_row) <- c("req_id", "tc_id")
-      this_row["tc_id"] <- trimws(this_row["tc_id"])
+  indiv_vals <- unlist(lapply(references$reference_insertion(cov_raw_values),
+                              strsplit, split = "\n"))
 
-      this_row
+  numbered_cov_vals <- as.data.frame(do.call("rbind", lapply(indiv_vals,
+    FUN = split_vals)))
 
-    })))
-  numbered_cov_vals <- numbered_cov_vals[order(numbered_cov_vals$req_id),]
-  row.names(numbered_cov_vals) <- 1:nrow(numbered_cov_vals)
-  numbered_cov_vals
+  if(type[1] == "long"){
+    numbered_cov_vals <- numbered_cov_vals[order(numbered_cov_vals$req_id),]
+    row.names(numbered_cov_vals) <- 1:nrow(numbered_cov_vals)
+    out_data <- numbered_cov_vals
+  } else if(type[1] == "wide"){
+    new_vals <- split_tc(numbered_cov_vals)
+    out_data <- make_wider(new_vals)
+  }
+  out_data
 }
--- tests/testthat/test-coverage-matrix.R
+++ tests/testthat/test-coverage-matrix.R
@@ -1,9 +1,12 @@
-test_that("coverage matrix, one file", {
+test_that("coverage matrix from dynam num", {
   withr::with_tempdir({
     vt_create_package(open = FALSE)
     vt_use_test_case("testcase1", username = "a user", open = FALSE)
     vt_use_test_case("testcase2", username = "a user", open = FALSE)
     vt_use_test_case("testcase3", username = "a user", open = FALSE)
+    vt_use_req("req1", username = "a user", open = FALSE)
+    vt_use_req("req2", username = "a user", open = FALSE)
+    vt_use_req("req3", username = "a user", open = FALSE)
 
     config_wd <- get_config_working_dir()
     cat(
@@ -59,7 +62,7 @@ test_that("coverage matrix, one file", {
         "  + T##tc:dynamic_numbering_testcase3.1 More testing. Matches requirements: ##req:dynamic_numbering3.1, ##req:dynamic_numbering3.3, and ##req:dynamic_numbering3.4",
         ""))
 
-    vt_add_file_to_config(c("testcase1.md", "testcase2.md", "testcase3.md"))
+    vt_add_file_to_config(c("req1.md", "testcase1.md", "req2.md", "testcase2.md", "req3.md", "testcase3.md"))
     cov_matrix <- vt_scrape_coverage_matrix()
     expect_matrix <- data.frame(req_id = as.vector(sapply(1:3, paste,  1:3, sep = ".")),
                                 tc_id = c("1.1, 1.2", "1.2, 1.3", "1.1, 1.3, 1.4",
@@ -68,9 +71,137 @@ test_that("coverage matrix, one file", {
     expect_equal(cov_matrix,
                  expect_matrix)
 
+    cov_matrix2 <- vt_scrape_coverage_matrix(type = "wide")
+    expect_matrix2 <- data.frame(req_id = c(rep("1.1", 2),
+                                            rep("1.2", 2),
+                                            rep("1.3", 3),
+                                            rep("2.1", 2),
+                                            rep("2.2", 2),
+                                            rep("2.3", 3),
+                                            rep("3.1", 2),
+                                            rep("3.2", 2),
+                                            rep("3.3", 3)),
+                                 `1.1` = c("x", rep("", 3), "x", rep("", 16)),
+                                 `1.2` = c("", rep("x", 2), rep("", 18)),
+                                 `1.3` = c(rep("", 3), "x", "", "x", rep("", 15)),
+                                 `1.4` = c(rep("", 6), "x", rep("", 14)),
+                                 `2.1` = c(rep("", 9), "x", "", "x", rep("", 9)),
+                                 `2.2` = c(rep("", 7), "x", rep("", 2), "x", rep("", 10)),
+                                 `2.3` = c(rep("", 8), "x", rep("", 3), "x", rep("", 8)),
+                                 `2.4` = c(rep("", 13), "x", rep("", 7)),
+                                 `3.1` = c(rep("", 14), "x", rep("", 3), "x", rep("", 2)),
+                                 `3.2` = c(rep("", 15), rep("x", 2), rep("", 4)),
+                                 `3.3` = c(rep("", 17), "x", "", "x", ""),
+                                 `3.4` = c(rep("", 20), "x"),
+                                 check.names = FALSE)
+
+    expect_equal(cov_matrix2,
+                 expect_matrix2)
+
 
   })
 })
 
+test_that("coverage matrix no dynam num", {
+  withr::with_tempdir({
+    vt_create_package(open = FALSE)
+    vt_use_test_case("testcase1", username = "a user", open = FALSE)
+    vt_use_test_case("testcase2", username = "a user", open = FALSE)
+    vt_use_test_case("testcase3", username = "a user", open = FALSE)
+    vt_use_req("req1", username = "a user", open = FALSE)
+    vt_use_req("req2", username = "a user", open = FALSE)
+    vt_use_req("req3", username = "a user", open = FALSE)
+
+    config_wd <- get_config_working_dir()
+    cat(
+      file = file.path(config_wd, "validation", "test_cases", "testcase1.md"),
+      sep = "\n",
+      c(
+        "#' @title Test Case 1",
+        "#' @editor User One",
+        "#' @editDate 2021-03-17",
+        "#' @coverage",
+        "#' 1.1: 1.1, 1.2",
+        "#' 1.2: 1.2, 1.3",
+        "#' 1.3: 1.1, 1.3, 1.4",
+        "",
+        "+ _Test Cases_",
+        "  + T1.1 Create a sample spec with a unique reference number. Matches requirements: 1.1 and 1.2",
+        "  + T1.2 Another test case. Matches requirements: 1.2 and 1.3",
+        "  + T1.3 More testing. Matches requirements: 1.1, 1.3, and 1.4",
+        ""))
+    cat(
+      file = file.path(config_wd, "validation", "test_cases", "testcase2.md"),
+      sep = "\n",
+      c(
+        "#' @title Test Case 2",
+        "#' @editor User One",
+        "#' @editDate 2021-03-17",
+        "#' @coverage",
+        "#' 2.2: 2.1, 2.2",
+        "#' 2.1: 2.2, 2.3",
+        "#' 2.3: 2.1, 2.3, 2.4",
+        "",
+        "+ _Test Cases_",
+        "  + T2.1 Create a sample spec with a unique reference number. Matches requirements: 2.1 and 2.2",
+        "  + T2.2 Another test case. Matches requirements: 2.2 and 2.3",
+        "  + T2.3 More testing. Matches requirements: 2.1, 2.3, and 2.4",
+        ""))
+
+    cat(
+      file = file.path(config_wd, "validation", "test_cases", "testcase3.md"),
+      sep = "\n",
+      c(
+        "#' @title Test Case 3",
+        "#' @editor User One",
+        "#' @editDate 2021-03-17",
+        "#' @coverage",
+        "#' 3.1: 3.1, 3.2",
+        "#' 3.2: 3.2, 3.3",
+        "#' 3.3: 3.1, 3.3, 3.4",
+        "",
+        "+ _Test Cases_",
+        "  + T3.3 Create a sample spec with a unique reference number. Matches requirements: 3.1 and 3.2",
+        "  + T3.2 Another test case. Matches requirements: 3.2 and 3.3",
+        "  + T3.1 More testing. Matches requirements: 3.1, 3.3, and 3.4",
+        ""))
+
+    vt_add_file_to_config(c("req1.md", "testcase1.md", "req2.md", "testcase2.md", "req3.md", "testcase3.md"))
+    cov_matrix <- vt_scrape_coverage_matrix()
+    expect_matrix <- data.frame(req_id = as.vector(sapply(1:3, paste,  1:3, sep = ".")),
+                                tc_id = c("1.1, 1.2", "1.2, 1.3", "1.1, 1.3, 1.4",
+                                          "2.2, 2.3", "2.1, 2.2", "2.1, 2.3, 2.4",
+                                          "3.1, 3.2", "3.2, 3.3", "3.1, 3.3, 3.4"))
+    expect_equal(cov_matrix,
+                 expect_matrix)
+
+    cov_matrix2 <- vt_scrape_coverage_matrix(type = "wide")
+    expect_matrix2 <- data.frame(req_id = c(rep("1.1", 2),
+                                            rep("1.2", 2),
+                                            rep("1.3", 3),
+                                            rep("2.1", 2),
+                                            rep("2.2", 2),
+                                            rep("2.3", 3),
+                                            rep("3.1", 2),
+                                            rep("3.2", 2),
+                                            rep("3.3", 3)),
+                                 `1.1` = c("x", rep("", 3), "x", rep("", 16)),
+                                 `1.2` = c("", rep("x", 2), rep("", 18)),
+                                 `1.3` = c(rep("", 3), "x", "", "x", rep("", 15)),
+                                 `1.4` = c(rep("", 6), "x", rep("", 14)),
+                                 `2.1` = c(rep("", 9), "x", "", "x", rep("", 9)),
+                                 `2.2` = c(rep("", 7), "x", rep("", 2), "x", rep("", 10)),
+                                 `2.3` = c(rep("", 8), "x", rep("", 3), "x", rep("", 8)),
+                                 `2.4` = c(rep("", 13), "x", rep("", 7)),
+                                 `3.1` = c(rep("", 14), "x", rep("", 3), "x", rep("", 2)),
+                                 `3.2` = c(rep("", 15), rep("x", 2), rep("", 4)),
+                                 `3.3` = c(rep("", 17), "x", "", "x", ""),
+                                 `3.4` = c(rep("", 20), "x"),
+                                 check.names = FALSE)
+
+    expect_equal(cov_matrix2,
+                 expect_matrix2)
 
 
+  })
+})
