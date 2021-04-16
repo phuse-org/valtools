653c890fb5408e3e4a81c5f2e3b3213432dbfd15
Ellis Hughes <ehhughes@scharp.org>
2021-04-16 10:59:33 -0700

    remove preference argument in find_files

--- tests/testthat/test-find_file.R
+++ tests/testthat/test-find_file.R
@@ -47,47 +47,6 @@ test_that("finding a file can return multiple files", {
   })
 })
 
-test_that("when finding multiple files a preference can be stated", {
-  withr::with_tempdir({
-
-    ## create folders
-    dir.create("folder1/folder2/folder3",recursive = TRUE)
-    dir.create("folder4/folder5/folder6",recursive = TRUE)
-    file.create("folder1/folder2/folder3/hello.R")
-    file.create("folder1/folder2/hello.R")
-    file.create("folder4/folder5/folder6/hello.R")
-
-    expect_equal(
-        find_file("hello.R", preference = "folder4"),
-        "folder4/folder5/folder6/hello.R"
-    )
-
-    expect_equal(
-      find_file("hello.R", preference = "folder5"),
-      "folder4/folder5/folder6/hello.R"
-    )
-
-    expect_equal(
-      find_file("hello.R", preference = "folder6"),
-      "folder4/folder5/folder6/hello.R"
-    )
-
-    expect_equal(
-      find_file("hello.R", preference = "folder2/folder3"),
-      "folder1/folder2/folder3/hello.R"
-    )
-
-    expect_equal(
-      find_file("hello.R", preference = "folder2"),
-      c(
-        "folder1/folder2/folder3/hello.R",
-        "folder1/folder2/hello.R"
-      )
-    )
-
-  })
-})
-
 
 
 
