4c452b1947656e4980c93711353c4b3b28e230e8
Peyman <peymaann@yahoo.co.uk>
2021-04-25 23:23:50 +0100

    shiny gadget for requirements

--- R/req.R
+++ R/req.R
@@ -1,11 +1,16 @@
 # The rest of this documentation is in case.R
 #' @param title Title for the requirement defaults to be the base name passed
 #'   sans file paths or extensions.
+#' @param use_shiny if \code{TRUE}, collect requirements details through a 
+#'   shiny gadget in RStudio's viewer pane.   
+#'   If RStudio or its viewer pane/dialog APIs are not available in the 
+#'   current R environment, then the gadget will be displayed in the system's 
+#'   default web browser.
 #'
 #' @rdname new_item
 #' @export
 vt_use_req <- function(name, username = vt_username(), title = NULL, open = interactive(),
-                       add_before = NULL, add_after = NULL){
+                       add_before = NULL, add_after = NULL, use_shiny = FALSE){
 
   # ensure file extensions are of the acceptable type
   name <- vt_set_ext(name, ext = "md")
@@ -17,9 +22,21 @@ vt_use_req <- function(name, username = vt_username(), title = NULL, open = inte
 
   ## if the file didnt exist before, populate with contents
   if (file.size(req_name) == 0){
+    
+    editDate <- as.character(Sys.Date())
 
     if(is.null(title)){
       title <- basename(file_path_sans_ext(req_name))
+    }
+    
+    reqs <- " "
+    
+    if(use_shiny){
+      reqList <- vt_use_req_shiny(name, username, title)
+      username <- reqList$username
+      title <- reqList$title
+      editDate <- reqList$editDate
+      reqs <- unname(rowSplit(reqList$reqs))
     }
 
     # Create the content to write
@@ -27,7 +44,8 @@ vt_use_req <- function(name, username = vt_username(), title = NULL, open = inte
                     data = list(
                       username = username,
                       title = title,
-                      editDate = as.character(Sys.Date())
+                      editDate = editDate,
+                      reqs = reqs
                     ))
   }
 
@@ -41,4 +59,58 @@ vt_use_req <- function(name, username = vt_username(), title = NULL, open = inte
 
 }
 
-
+#' provide a shiny gadget interface to collect  requirements
+#'
+#' This function is not intended for use by the end users, and is only called 
+#' internally by vt_use_req()
+#' 
+#' @param name name from vt_use_req()
+#' @param username username from vt_use_req()
+#' @param title title from vt_use_req()
+#' 
+#' @noRd
+vt_use_req_shiny <- function(name, username, title){
+  
+  dfReqs <- data.frame(ID = "1.1",
+                  Requirement = "Start typing here to document requirements.\n Right click to insert or remove rows.",
+                  stringsAsFactors = FALSE)
+  
+  
+  # Our ui will display a simple gadget page to collect 
+  # user input
+  ui <- miniUI::miniPage(
+    miniUI::gadgetTitleBar(name),
+    miniUI::miniContentPanel(
+      shiny::textInput("title", "Title:", value= title),
+      shiny::textInput("username", "Editor's name:", value= username),
+      shiny::textInput("editDate", "Edit date:", value = as.character(Sys.Date())),
+      rhandsontable::rHandsontableOutput("reqTable")
+    )
+  )
+  
+  server <- function(input, output, session) {
+    rvReqs <- reactiveValues(data= dfReqs)
+    output$reqTable <- rhandsontable::renderRHandsontable(
+      rhandsontable::rhandsontable(rvReqs$data, rowHeaders = NULL))
+    
+    #Saves the added or removed row 
+    observeEvent(input$reqTable,{
+      rvReqs$data <- rhandsontable::hot_to_r(input$reqTable)
+    })
+    
+    # Listen for 'done' events. When we're finished, we'll
+    # pass on the data in reqList list.
+    shiny::observeEvent(input$done, {
+      lsReqs <- list(username = input$username,
+                      title = input$title,
+                      editDate = input$editDate,
+                      reqs = rvReqs$data)
+      shiny::stopApp(lsReqs)
+    })
+    
+  }
+  
+  # Use a paneViewer, and set the minimum height at 400
+  viewer <- shiny::paneViewer('maximize')
+  shiny::runGadget(ui, server, viewer = viewer)
+}
