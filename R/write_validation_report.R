67cfd0d6f8c15fd1549bb7f599f3f8e8f49c2148
Marie Vendettuoli <mvendett@scharp.org>
2021-04-27 17:48:52 -0700

    find child files

---
+++ R/write_validation_report.R
@@ -0,0 +1,92 @@
+#' Create validation report from template
+#' @param pkg_name name of package
+#' @param template what validation report template from {valtools} to use,
+#' passed to \code{usethis::use_template}
+#' @param open boolean whether to open the validation report, passed to \code{usetthis::use_template}
+#' @importFrom usethis use_template proj_set
+#' @importFrom tools file_ext
+#' @importFrom rlang with_interactive
+#' @export
+vt_use_report <- function(pkg_name = desc::desc_get_field("Package"),
+                          template = "validation_report.Rmd",
+                          open = FALSE){
+
+  tryCatch({
+    dir <- vt_find_config()
+  },
+  error = function(e){
+    vt_use_config(pkg = vt_path())
+    dir <- vt_find_config()
+  })
+
+  val_leads <- get_val_leads(dir = dirname(dir))
+
+  if(length(val_leads) == 0){
+    val_leads <-  tryCatch({
+      val_leads_username <- with_interactive(value = FALSE, vt_username())
+      val_leads <- vt_get_user_info(username = val_leads_username,
+                                        type = "name")[["name"]]
+
+      config_details <- read_validation_config()
+      old_role <- config_details$usernames[[val_leads]]$role
+      config_details$usernames[[val_leads]]$role <-
+        ifelse(grepl(pattern = "validation lead", x = tolower(old_role)),
+               old_role,
+               paste0(c("Validation Lead", old_role ), collapse = ", "))
+      write_validation_config(path = dir,
+                              working_dir = config_details$working_dir,
+                              output_dir = config_details$output_dir,
+                              report_naming_format = config_details$report_naming_format,
+                              username_list = config_details$usernames)
+      return(val_leads)
+      }, error = function(e){
+        val_leads <- username(fallback = "")
+        vt_add_user_to_config(username = val_leads, name = val_leads, title = "",
+                              role = "Validation Lead")
+
+
+        inform(
+          message = paste0("User `", val_leads, "` does not exist in the config file.  ",
+                           "Assigning to role of `Validation Lead`"),
+          class = "vt.validation_config_missing_user_inform"
+        )
+        return(val_leads)
+      })
+  }
+
+
+  render_template( template = template,
+                output = file.path(get_config_working_dir(),
+                                   paste0(evaluate_filename(), ".", file_ext(template))),
+                data = list(pkg_name = desc_get_field("Package", file = dirname(dir)),
+                         title = "Validation Report",
+                         author = paste0((sapply(val_leads,
+                                                 vt_get_user_info,
+                                                 type = "name")), collapse = ', ')))
+
+
+  file_list <- get_config_validation_files()
+  file_list
+}
+
+#' @noRd
+#' @keywords internal
+get_val_leads <- function(dir = "."){
+
+
+  if(!file.exists(file.path(dir,"validation.yml"))){
+    abort(
+      paste0(
+        "A validation config file does not exist.\n",
+        "Run `valtools::vt_use_validation_config()` to create a validation config file."
+      ),
+      class = "vt.validation_config_missing"
+    )
+  }
+  usernames <- read_validation_config()$usernames
+  unlist(lapply(seq_along(usernames), FUN = function(x){
+    if(grepl(pattern = "validation lead", x = tolower(usernames[[x]]$role))){
+      return(names(usernames)[x])
+    }
+  }))
+}
---
+++ tests/testthat/test-write_validation_report.R
@@ -0,0 +1,203 @@
+test_that("integration test for CRAN", {
+  withr::with_tempdir({
+    # using the default .validation Validation Lead user
+    test_user <- whoami::username(fallback = "")
+    report_name <- paste0("Validation_Report_", basename(getwd()),
+                          "_v0.0.0.9000_", format(Sys.Date(), "%Y%m%d"), ".Rmd")
+    captured_output <- capture.output(vt_create_package(open = FALSE))
+    vt_add_user_to_config(username = "auser", name = "A user", title = "staff",
+                          role = "Project Lead")
+    vt_add_user_to_config(username = "buser", name = "B user", title = "staff",
+                          role = "Write Requirements")
+    vt_add_user_to_config(username = "cuser", name = "C user", title = "staff",
+                          role = "Write Test Cases, Tester")
+    vt_add_user_to_config(username = "duser", name = "D user", title = "staff",
+                          role = "Write Code")
+    vt_use_change_log(date = "2021-01-01", open = FALSE)
+    vt_use_req("req1.md", username = "B user", title = "##req:req1", open = FALSE)
+    writeLines(con = file.path("vignettes", "validation", "requirements", "req1.md"),
+               c(
+                 "#' @title ##req:req1",
+                 "#' @editor B user",
+                 "#' @editDate 2021-04-27",
+                 "#' @riskAssessment",
+                 "#' REQUIREMENT: ASSESSMENT",
+                 "\n",
+                 "## Requirement ##req:req1",
+                 "\n",
+                 "+ Requirement text",
+                 "  - More text",
+                 "\n")
+    )
+    vt_use_req("req2.md", username = "B user", title = "##req:req2", open = FALSE)
+    writeLines(con = file.path("vignettes", "validation", "requirements", "req2.md"),
+               c(
+                 "#' @title ##req:req2",
+                 "#' @editor B user",
+                 "#' @editDate 2021-04-27",
+                 "#' @riskAssessment",
+                 "#' REQUIREMENT: ASSESSMENT",
+                 "\n",
+                 "## Requirement ##req:req2",
+                 "\n",
+                 "+ Requirement text",
+                 "  - More text",
+                 "\n")
+    )
+    vt_use_test_case("test_case1.md", username = "C User", title = "##tc:tc1", open = FALSE)
+    writeLines(con = file.path("vignettes", "validation", "test_cases", "test_case1.md"),
+               c(
+               "#' @title ##tc:tc1",
+               "#' @editor C User",
+               "#' @editDate 2021-03-26",
+               "#' @coverage",
+               "#' ##tc1.1: ##req1.1, ##req1.2",
+               "#' ##tc1.2: ##req1.3",
+               "#' ##tc1.3: ##req1.4",
+               "",
+               "##  Test Case ##tc:tc1",
+               "+ Setup: DOCUMENT ANY SETUP THAT NEEDS TO BE DONE FOR TESTING",
+               "",
+               "+ Start documenting test case here!"))
+
+    file.create(file.path("R", "hello_world.R"))
+    writeLines(con = file.path("R", "hello_world.R"),
+               c("#' dummy function",
+                 "#' @param name person name",
+                 "#' @return greeting",
+                 "#' @editor An author",
+                 "#' @editDate 2021-01-01",
+                 "#' @export",
+                 " hello_world <- function(name){",
+                 "   paste('Hello', name, '!')",
+                 " }",
+                 "\n\n",
+                 "#' dummy function2",
+                 "#' @param name person name",
+                 "#' @return greeting with date",
+                 "#' @editor Another author",
+                 "#' @editDate 2021-01-01",
+                 "hello_world2 <- function(name){",
+                 "  paste0('Hello ', name, ', today is: ', Sys.Date(), '!')",
+                 "}"
+               ))
+
+
+    vt_use_report()
+    report_code <- readLines(file.path(getwd(), "vignettes", report_name))
+    withr::with_temp_libpaths({
+      install.packages(getwd(), type = "source", repos = NULL)
+      browser()
+      rmarkdown::render(file.path(getwd(), "vignettes", report_name))
+    })
+    expect_true(file.exists(file.path(getwd(),"vignettes", report_name)))
+    # lines in rmd template that are updated via vt_use_report calls
+    expect_equal(report_code[2], "title: Validation Report")
+    expect_equal(report_code[3], paste0("author: ", test_user))
+    expect_equal(report_code[10], "  %\\VignetteIndexEntry{ Validation Report }")
+    expect_equal(report_code[25], paste0("  library(", basename(getwd()), ")"))
+
+
+  })
+})
+
+test_that("validation report in package",{
+  skip_on_cran()
+  withr::with_tempdir({
+    # using the default .validation Validation Lead user
+    test_user <- whoami::username(fallback = "")
+    report_name <- paste0("Validation_Report_", basename(getwd()),
+                          "_v0.0.0.9000_", format(Sys.Date(), "%Y%m%d"), ".Rmd")
+    captured_output <- capture.output(vt_create_package(open = FALSE))
+    vt_use_report()
+    report_code <- readLines(file.path(getwd(), "vignettes", report_name))
+
+    expect_true(file.exists(file.path(getwd(),"vignettes", report_name)))
+    # lines in rmd template that are updated via vt_use_report calls
+    expect_equal(report_code[2], "title: Validation Report")
+    expect_equal(report_code[3], paste0("author: ", test_user))
+    expect_equal(report_code[10], "  %\\VignetteIndexEntry{ Validation Report }")
+    expect_equal(report_code[25], paste0("  library(", basename(getwd()), ")"))
+
+  })
+})
+
+test_that("validation report in package",{
+  skip_on_cran()
+  withr::with_tempdir({
+    # using the default .validation Validation Lead user
+
+    report_name <- paste0("Validation_Report_", basename(getwd()),
+                          "_v0.0.0.9000_", format(Sys.Date(), "%Y%m%d"), ".Rmd")
+    captured_output <- capture.output({vt_create_package(open = FALSE)})
+    vt_add_user_to_config(username = "aperson",
+                          name = "An author",
+                          title = "Programmer",
+                          role = "Validation Lead")
+    vt_use_report()
+    report_code <- readLines(file.path(getwd(), "vignettes", report_name))
+
+    expect_true(file.exists(file.path(getwd(),"vignettes", report_name)))
+    # lines in rmd template that are updated via vt_use_report calls
+    expect_equal(report_code[3], paste0("author: An author"))
+
+
+  })
+})
+
+
+test_that("multiple authors",{
+  skip_on_cran()
+  withr::with_tempdir({
+    # using the default .validation Validation Lead user
+
+    report_name <- paste0("Validation_Report_", basename(getwd()),
+                          "_v0.0.0.9000_", format(Sys.Date(), "%Y%m%d"), ".Rmd")
+    captured_output <- capture.output(vt_create_package(open = FALSE))
+    vt_add_user_to_config(username = "aperson",
+                          name = "An author",
+                          title = "Programmer",
+                          role = "Validation Lead")
+
+    vt_add_user_to_config(username = "bperson",
+                          name = "Another author",
+                          title = "Something",
+                          role = "Validation Lead Designee")
+
+    vt_add_user_to_config(username = "cperson",
+                          name = "Not the validation lead",
+                          title = "Manager",
+                          role = "Manager")
+    vt_use_report()
+    report_code <- readLines(file.path(getwd(), "vignettes", report_name))
+
+    expect_true(file.exists(file.path(getwd(),"vignettes", report_name)))
+    # lines in rmd template that are updated via vt_use_report calls
+    expect_equal(report_code[3], paste0("author: An author, Another author"))
+
+
+  })
+})
+
+
+
+
+test_that("multiple authors",{
+  skip_on_cran()
+  withr::with_tempdir({
+    # using the default Validation Lead user
+    test_user <- whoami::username(fallback = "")
+    report_name <- paste0("Validation_Report_", basename(getwd()),
+                          "_v0.0.0.9000_", format(Sys.Date(), "%Y%m%d"), ".Rmd")
+    captured_output <- capture.output({vt_create_package(open = FALSE)})
+
+    vt_use_req("requirement1.md", username = "author1", open = FALSE)
+    vt_use_test_case("testcase1.md", username = "author1", open = FALSE)
+
+    vt_use_report()
+    report_code <- readLines(file.path(getwd(), "vignettes", report_name))
+
+
+  })
+})
+
