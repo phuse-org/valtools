f8f1ba4dc5f7510e89cd990b33e2b1ede73d2821
Ellis Hughes <ehhughes@scharp.org>
2021-05-04 14:17:12 -0700

    change log handler reversion

--- R/change_log_handlers.R
+++ R/change_log_handlers.R
@@ -75,15 +75,14 @@ vt_scrape_change_log <- function(){
 #'  vt_kable_change_log(log_data)
 #'
 #' })
-vt_kable_change_log <- function(change_log_info,format = vt_render_to()){
+vt_kable_change_log <- function(change_log_info, format = "latex"){
   all_news <- change_log_info[order(change_log_info$version, decreasing = TRUE),
                         c("version", "effective_date", "description")]
   rownames(all_news) <- 1:nrow(all_news)
-  colnames(all_news) <- c("Version", "Effective Date", "Activity Description")
-
 
   t <- kable(all_news,
-             format = format )
+             format = format,
+             col.names = c("Version", "Effective Date", "Activity Description"))
   t <- column_spec(t, 1, border_left = TRUE)
   t <- column_spec(t, 3, border_right = TRUE, width = "30em")
   t <- collapse_rows(t, 1:2)
