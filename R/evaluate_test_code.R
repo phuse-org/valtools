d7fe8338a3034f3df32ce081568c5fbec214275d
Jan Meis <meis@imbi.uni-heidelberg.de>
2022-10-14 14:42:50 +0200

    hopefully get longtable to work

--- R/evaluate_test_code.R
+++ R/evaluate_test_code.R
@@ -60,7 +60,7 @@ vt_kable_test_code_results <- function(results, format = vt_render_to()) {
     x$Results <- gsub("\n","",x$Results)
   }
 
-  t <- kable(x,format = format)
+  t <- kable(x, format = format, longtable = TRUE)
 
   if(nrow(results) > 0){
     t <- column_spec(t, 2:3, width = "10em")
