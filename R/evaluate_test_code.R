1e1978394dab6763d8b3416f381c2c18396ffabe
Lukas D. Sauer <lukasdsauer@yahoo.de>
2022-12-07 10:11:38 +0100

    Update evaluate_test_code.R
    Incorporating the bug fix for tests that failed, see https://github.com/phuse-org/valtools/pull/222/files

--- R/evaluate_test_code.R
+++ R/evaluate_test_code.R
@@ -57,7 +57,7 @@ vt_kable_test_code_results <- function(results, format = vt_render_to()) {
   x <- results[, c("Test", "Results", "Pass_Fail")]
   colnames(x) <- c("Test", "Results", "Pass/Fail")
 
-  t <- kable(x,format = format)
+  t <- kable(x, format = format, longtable = TRUE)
 
   if(nrow(results) > 0){
     t <- column_spec(t, 2:3, width = "10em")
